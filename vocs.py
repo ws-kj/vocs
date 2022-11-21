bbdc0abc5daf73619e3668646d47b6b74d797476
Will Savage <wsavage6316@gmail.com>
2022-11-20 22:39:28 -0500

    added api connection

---
+++ vocs.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = [
+    'https://www.googleapis.com/auth/documents'
+]
+
+def conn():
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('docs', 'v1', credentials=creds)
+        print("connected to docs api")
+        return service
+    except HttpError as err:
+        print(err)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    conn()
