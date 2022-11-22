796bba6c9b67235e23b6de6abfe78afc9d5030a3
Will Savage <wsavage6316@gmail.com>
2022-11-22 12:47:45 -0500

    full doc push udates

--- vocs.py
+++ vocs.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 
@@ -12,12 +13,28 @@ SCOPES = [
     'https://www.googleapis.com/auth/drive'
 ]
 
+class Document(object):
+    def __init__(self, docid, revision, body=None):
+        self.docid = docid
+        self.revision = revision
+        if body is not None:
+            self.body = body
+            self.start_index = 1
+            self.end_index = len(body)-1
+
+    def update_body(self, body):
+        self.body = body
+        self.start_index = 1
+        self.end_index = len(body)-1
+
 class APIClient(object):
 
     def __init__(self):
         self.docs_service = None
         self.drive_service = None
         self.creds = None
+
+        self.current_doc = None
 
         if os.path.exists('token.json'):
             self.creds = Credentials.from_authorized_user_file('token.json', SCOPES)
@@ -29,7 +46,7 @@ class APIClient(object):
                     'credentials.json', SCOPES)
                 self.creds = flow.run_local_server(port=0)
             with open('token.json', 'w') as token:
-                token.write(creds.to_json())    
+                token.write(self.creds.to_json())    
         try:
             self.drive_service = build('drive', 'v3', credentials=self.creds) 
             self.docs_service = build('docs', 'v1', credentials=self.creds)
@@ -40,15 +57,17 @@ class APIClient(object):
 
     def load_doc(self, docid):
         try:
-            doc = self.docs_service.documents().get(documentId=docid).execute()
-            return doc
+            doc_resp = self.docs_service.documents().get(documentId=docid).execute()
+            body = self.build_raw(doc_resp)
+            revision = doc_resp["revisionId"]
+            self.current_doc = Document(docid, revision, body)
+
         except HttpError as err:
             print(err)
-            return {}
 
-    def build_raw(self, doc):
+    def build_raw(self, doc_resp):
         raw = ""
-        for elem in doc["body"]["content"]:
+        for elem in doc_resp["body"]["content"]:
             if "paragraph" in elem:
                 raw += elem["paragraph"]["elements"][0]["textRun"]["content"]
         return raw
@@ -74,13 +93,44 @@ class APIClient(object):
            
         return files
 
+    def push_update(self):
+        if self.current_doc == None or self.current_doc.body == None:
+            return
+        try:
+            batch = self.docs_service.new_batch_http_request();
+
+            response = self.docs_service.documents().batchUpdate(
+                documentId=self.current_doc.docid,
+                body={"requests": [
+                    { "deleteContentRange": {
+                        "range": {
+                            "startIndex": self.current_doc.start_index,
+                            "endIndex": self.current_doc.end_index
+                        }
+                    }},
+                    { "insertText": {
+                        "location": {
+                            "index": 1
+                        },
+                        "text": self.current_doc.body
+                    }}
+                ]}
+            ).execute()
+
+            if "writeControl" in response:
+                self.current_doc.revision = response["writeControl"]["requiredRevisionId"]
+
+        except HttpError as err:
+            print(err)         
+                            
+
 if __name__ == '__main__':
     client = APIClient()
 
     if len(sys.argv) > 1:
-        doc = client.load_doc(sys.argv[1])
-
-        print(doc['title'] + "\n\n")
-        print(client.build_raw(doc))
+        client.load_doc(sys.argv[1])
+        client.current_doc.body = client.current_doc.body + client.current_doc.body
+        client.push_update()
+        
     else:
         print(client.get_files())
